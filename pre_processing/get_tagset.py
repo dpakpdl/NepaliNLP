9744eed9b86772f5e6fb09f62c04ef856096133f
deepak p <>
2019-07-30 13:48:52 +0545

    handled file not found error

--- pre_processing/get_tagset.py
+++ pre_processing/get_tagset.py
@@ -1,8 +1,13 @@
+import os
+
 import pandas as pd
+
+THIS_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 def read():
-    data = pd.read_csv("sentences.csv", encoding="utf-8")
+    file_path = os.path.join(THIS_DIR, 'sentences.csv')
+    data = pd.read_csv(file_path, encoding="utf-8")
     data = data.fillna(method="ffill")
     print(data.tail(10))
     return data
