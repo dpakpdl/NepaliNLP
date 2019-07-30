f9477657f26989b865814a7d8a3ce48897deffd6
deepak p <>
2019-07-30 11:46:45 +0545

    preprocessing for sentences added

---
+++ pre_processing/get_sentences.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+
+class SentenceGetter(object):
+
+    def __init__(self, data):
+        self.n_sent = 1
+        self.data = data
+        self.empty = False
+        self.grouped = self.data.groupby("sentence").apply(self.aggregate)
+        self.sentences = [s for s in self.grouped]
+
+    @staticmethod
+    def aggregate(s):
+        return [(word, pos) for word, pos in zip(s["word"].values.tolist(), s["pos"].values.tolist())]
+
+    def get_next(self):
+        try:
+            s = self.grouped["sentence: {}".format(self.n_sent)]
+            self.n_sent += 1
+            return s
+        except Exception as ex:
+            print (ex)
+            return None
+
+
+def main():
+    data = pd.read_csv("sentences.csv", encoding="utf-8")
+    data = data.fillna(method="ffill")
+    print(data.tail(10))
+    getter = SentenceGetter(data)
+    print(getter.sentences)
+
+
+if __name__ == "__main__":
+    main()
