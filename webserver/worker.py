59d1ffe49fd826f9559d685e3c33d26966fa9ec4
Deepak Paudel <dipak93@hotmail.com>
2020-01-27 17:18:51 +0545

    Update worker.py

--- webserver/worker.py
+++ webserver/worker.py
@@ -9,7 +9,7 @@ listen = ['default']
 redis_url = "redis-12862.c93.us-east-1-3.ec2.cloud.redislabs.com"
 
 # conn = redis.from_url(redis_url)
-conn = redis.Redis(host=redis_url, port=12862, db="nepali-nlp", password="rtw5ZgU4fbtqJ44cyIGEQfQtMHvaHhWq")
+conn = redis.Redis(host=redis_url, port=12862, password="rtw5ZgU4fbtqJ44cyIGEQfQtMHvaHhWq")
 
 
 if __name__ == '__main__':
