d1ba269bc207a60bf3408c658a14ec4a84b3277c
Deepak Paudel <dipak93@hotmail.com>
2020-01-27 17:04:36 +0545

    Update worker.py

--- webserver/worker.py
+++ webserver/worker.py
@@ -5,9 +5,12 @@ from rq import Worker, Queue, Connection
 
 listen = ['default']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+# redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = "redis-12862.c93.us-east-1-3.ec2.cloud.redislabs.com"
 
-conn = redis.from_url(redis_url)
+# conn = redis.from_url(redis_url)
+conn = redis.Redis(host=redis_url, port=12862, db="nepali-nlp", password="rtw5ZgU4fbtqJ44cyIGEQfQtMHvaHhWq")
+
 
 if __name__ == '__main__':
     with Connection(conn):
